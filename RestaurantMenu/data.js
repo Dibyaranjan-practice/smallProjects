07da9473b42efa52448e0b2d17252108b53b7941
Dibya <selfstudyhack@gmail.com>
2023-10-15 22:46:01 +0530

    order menu progress

--- RestaurantMenu/data.js
+++ RestaurantMenu/data.js
@@ -1,11 +1,12 @@
 const items = [
-  [0, "biriyani", "price", "description", "./images/biriyani.jpg"],
-  [1, "chicken-kasa", "price", "description", "./images/chicken-kasa.jpg"],
-  [2, "mushroom-rice", "price", "description", "./images/mushroom-rice.jpg"],
-  [3, "chicken-kasa", "price", "description", "./images/chicken-kasa.jpg"],
-  [4, "biriyani", "price", "description", "./images/biriyani.jpg"],
-  [5, "mushroom-rice", "price", "description", "./images/mushroom-rice.jpg"],
-  [6, "palao", "price", "description", "./images/palao.jpg"],
-  [7, "chicken-kasa", "price", "description", "./images/chicken-kasa.jpg"],
-  [8, "biriyani", "price", "description", "./images/biriyani.jpg"],
+  [0, "biriyani", "100", "description", "./images/biriyani.jpg"],
+  [1, "chicken-kasa", "100", "description", "./images/chicken-kasa.jpg"],
+  [2, "mushroom-rice", "100", "description", "./images/mushroom-rice.jpg"],
+  [3, "chicken-kasa", "100", "description", "./images/chicken-kasa.jpg"],
+  [4, "biriyani", "100", "description", "./images/biriyani.jpg"],
+  [5, "mushroom-rice", "100", "description", "./images/mushroom-rice.jpg"],
+  [6, "palao", "100", "description", "./images/palao.jpg"],
+  [7, "chicken-kasa", "100", "description", "./images/chicken-kasa.jpg"],
+  [8, "biriyani", "100", "description", "./images/biriyani.jpg"],
 ];
+  
